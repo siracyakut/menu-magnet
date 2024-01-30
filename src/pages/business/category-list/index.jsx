e794ba8f4c8ac2e27b14272b74e27789e6bb925f
Siraç Yakut <sirac1234@gmail.com>
2024-01-30 18:23:04 +0300

    :sparkles: Added react-helmet, fixed business page
    React-helmet package was included in the project and page titles were improved. Additionally, the error that occurred when no menu was created on the business page was fixed.

--- src/pages/business/category-list/index.jsx
+++ src/pages/business/category-list/index.jsx
@@ -51,96 +51,100 @@ export default function CategoryList({ businessId, color }) {
     }
   };
 
-  return isFetching || menuFetching ? (
-    <Loading inline={true} />
-  ) : error || menuError ? (
-    <ErrorBox>{error.data || menuError.data}</ErrorBox>
-  ) : (
-    <div className="flex flex-col gap-12">
-      {Object.entries(menus).map(([key, value], index) => (
-        <div key={index} className="flex flex-col gap-4">
-          <Disclosure defaultOpen={true}>
-            {({ open }) => (
-              <>
-                <Disclosure.Button className="flex items-center justify-between gap-x-4 bg-white/10 border border-white/30 p-2.5 rounded-lg outline-none">
-                  <div className="flex flex-col gap-1.5">
-                    <div className="flex items-center gap-x-3">
-                      <FontAwesomeIcon
-                        className="flex-shrink-0"
-                        icon={ICONS.at(
-                          data.data.find((d) => d._id === key).icon,
-                        )}
-                        size="xl"
-                      />
-                      <p className="text-left text-xl md:text-2xl font-extrabold break-all">
-                        {data.data.find((d) => d._id === key).name}
-                      </p>
-                    </div>
-                    <p className="text-left text-sm font-bold">
-                      {data.data.find((d) => d._id === key).description}
-                    </p>
-                  </div>
-                  {open ? (
-                    <FaChevronUp className="flex-shrink-0" size={20} />
-                  ) : (
-                    <FaChevronDown className="flex-shrink-0" size={20} />
-                  )}
-                </Disclosure.Button>
-                <Transition
-                  enter="transition duration-300 ease-out"
-                  enterFrom="transform scale-95 opacity-0"
-                  enterTo="transform scale-100 opacity-100"
-                  leave="transition duration-225 ease-out"
-                  leaveFrom="transform scale-100 opacity-100"
-                  leaveTo="transform scale-95 opacity-0"
-                >
-                  <Disclosure.Panel className="grid gap-4 px-4">
-                    {value.map((menu, i) => (
-                      <div
-                        key={i}
-                        className="flex flex-col gap-4 bg-white/10 border border-white/30 p-4 rounded-lg break-all"
-                      >
-                        <div className="flex items-center justify-between gap-x-2">
-                          <div className="flex items-center gap-x-4">
-                            <p
-                              style={{
-                                "--color": color,
-                                "--text": getHSL(color),
-                              }}
-                              className="flex-shrink-0 w-8 h-8 md:w-10 md:h-10 rounded-full bg-[var(--color)] text-[var(--text)] flex items-center justify-center font-extrabold"
-                            >
-                              {i + 1}
-                            </p>
-                            <p className="text-2xl font-extrabold">
-                              {menu.name}
-                            </p>
-                          </div>
-                          <FaHeart
-                            onClick={() => handleFavorite(menu)}
-                            size={24}
-                            className={classNames(
-                              "flex-shrink-0 text-white cursor-pointer",
-                              {
-                                "!text-red-600": favorites.find(
-                                  (f) => f._id === menu._id,
-                                ),
-                              },
-                            )}
-                          />
-                        </div>
-                        <p className="text-sm font-bold">{menu.description}</p>
-                        <p className="text-sm font-bold">
-                          {formatMoney(menu.price)}
+  return !isFetching && !menuFetching ? (
+    !error && !menuError ? (
+      <div className="flex flex-col gap-12">
+        {Object.entries(menus).map(([key, value], index) => (
+          <div key={index} className="flex flex-col gap-4">
+            <Disclosure defaultOpen={true}>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button className="flex items-center justify-between gap-x-4 bg-white/10 border border-white/30 p-2.5 rounded-lg outline-none">
+                    <div className="flex flex-col gap-1.5">
+                      <div className="flex items-center gap-x-3">
+                        <FontAwesomeIcon
+                          className="flex-shrink-0"
+                          icon={ICONS.at(
+                            data.data.find((d) => d._id === key).icon,
+                          )}
+                          size="xl"
+                        />
+                        <p className="text-left text-xl md:text-2xl font-extrabold break-all">
+                          {data.data.find((d) => d._id === key).name}
                         </p>
                       </div>
-                    ))}
-                  </Disclosure.Panel>
-                </Transition>
-              </>
-            )}
-          </Disclosure>
-        </div>
-      ))}
-    </div>
+                      <p className="text-left text-sm font-bold">
+                        {data.data.find((d) => d._id === key).description}
+                      </p>
+                    </div>
+                    {open ? (
+                      <FaChevronUp className="flex-shrink-0" size={20} />
+                    ) : (
+                      <FaChevronDown className="flex-shrink-0" size={20} />
+                    )}
+                  </Disclosure.Button>
+                  <Transition
+                    enter="transition duration-300 ease-out"
+                    enterFrom="transform scale-95 opacity-0"
+                    enterTo="transform scale-100 opacity-100"
+                    leave="transition duration-225 ease-out"
+                    leaveFrom="transform scale-100 opacity-100"
+                    leaveTo="transform scale-95 opacity-0"
+                  >
+                    <Disclosure.Panel className="grid gap-4 px-4">
+                      {value.map((menu, i) => (
+                        <div
+                          key={i}
+                          className="flex flex-col gap-4 bg-white/10 border border-white/30 p-4 rounded-lg break-all"
+                        >
+                          <div className="flex items-center justify-between gap-x-2">
+                            <div className="flex items-center gap-x-4">
+                              <p
+                                style={{
+                                  "--color": color,
+                                  "--text": getHSL(color),
+                                }}
+                                className="flex-shrink-0 w-8 h-8 md:w-10 md:h-10 rounded-full bg-[var(--color)] text-[var(--text)] flex items-center justify-center font-extrabold"
+                              >
+                                {i + 1}
+                              </p>
+                              <p className="text-2xl font-extrabold">
+                                {menu.name}
+                              </p>
+                            </div>
+                            <FaHeart
+                              onClick={() => handleFavorite(menu)}
+                              size={24}
+                              className={classNames(
+                                "flex-shrink-0 text-white cursor-pointer",
+                                {
+                                  "!text-red-600": favorites.find(
+                                    (f) => f._id === menu._id,
+                                  ),
+                                },
+                              )}
+                            />
+                          </div>
+                          <p className="text-sm font-bold">
+                            {menu.description}
+                          </p>
+                          <p className="text-sm font-bold">
+                            {formatMoney(menu.price)}
+                          </p>
+                        </div>
+                      ))}
+                    </Disclosure.Panel>
+                  </Transition>
+                </>
+              )}
+            </Disclosure>
+          </div>
+        ))}
+      </div>
+    ) : (
+      <ErrorBox>{error?.data || menuError?.data}</ErrorBox>
+    )
+  ) : (
+    <Loading inline={true} />
   );
 }
--- src/pages/business/index.jsx
+++ src/pages/business/index.jsx
@@ -12,6 +12,7 @@ import { useModal } from "~/store/modal/hooks";
 import Modal from "~/modals";
 import { useFavorites } from "~/store/favorites/hooks";
 import toast, { Toaster } from "react-hot-toast";
+import { Helmet } from "react-helmet";
 
 export default function BusinessPage() {
   const { modal } = useModal();
@@ -27,53 +28,60 @@ export default function BusinessPage() {
   ) : error ? (
     <ErrorBox>{error.data}</ErrorBox>
   ) : (
-    <div className="flex flex-col gap-10">
-      {modal && <Modal />}
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          style: {
-            borderRadius: "10px",
-            background: "#333",
-            color: "#fff",
-            fontWeight: "bold",
-          },
-        }}
-      />
-      <header
-        style={{
-          "--color": COLORS.at(data.data.color),
-          "--text": getHSL(COLORS.at(data.data.color)),
-        }}
-        className="w-full p-5 bg-[var(--color)] text-[var(--text)] flex items-center justify-center text-center"
-      >
-        <p className="text-4xl font-extrabold">{data.data.name}</p>
-      </header>
-      <div className="md:max-w-[1030px] w-full mx-auto px-5">
-        <CategoryList
-          businessId={data.data._id}
-          color={COLORS.at(data.data.color)}
+    <>
+      <Helmet>
+        <title>{data.data.name}</title>
+      </Helmet>
+      <div className="flex flex-col gap-10">
+        {modal && <Modal />}
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              borderRadius: "10px",
+              background: "#333",
+              color: "#fff",
+              fontWeight: "bold",
+            },
+          }}
         />
+        <header
+          style={{
+            "--color": COLORS.at(data.data.color),
+            "--text": getHSL(COLORS.at(data.data.color)),
+          }}
+          className="w-full p-5 bg-[var(--color)] text-[var(--text)] flex items-center justify-center text-center"
+        >
+          <p className="text-4xl font-extrabold">{data.data.name}</p>
+        </header>
+        <div className="md:max-w-[1030px] w-full mx-auto px-5">
+          <CategoryList
+            businessId={data.data._id}
+            color={COLORS.at(data.data.color)}
+          />
+        </div>
+        <button
+          onClick={() => {
+            if (favorites.length > 0) {
+              setModal("favorites");
+            } else {
+              toast.error("You haven't chosen any favorite menu!");
+            }
+          }}
+          type="button"
+          className="fixed bottom-5 right-5 bg-secondary p-4 rounded-[15px] z-10 shadow-primaryShadow flex items-center gap-x-4 outline-none"
+        >
+          <FaHeart className="text-red-600" size={22} />
+          <p className="hidden md:block text-xl font-bold">
+            My Favorites{" "}
+            {favorites.length > 0 && (
+              <span className="hidden md:inline-block">
+                ({favorites.length})
+              </span>
+            )}
+          </p>
+        </button>
       </div>
-      <button
-        onClick={() => {
-          if (favorites.length > 0) {
-            setModal("favorites");
-          } else {
-            toast.error("You haven't chosen any favorite menu!");
-          }
-        }}
-        type="button"
-        className="fixed bottom-5 right-5 bg-secondary p-4 rounded-[15px] z-10 shadow-primaryShadow flex items-center gap-x-4 outline-none"
-      >
-        <FaHeart className="text-red-600" size={22} />
-        <p className="hidden md:block text-xl font-bold">
-          My Favorites{" "}
-          {favorites.length > 0 && (
-            <span className="hidden md:inline-block">({favorites.length})</span>
-          )}
-        </p>
-      </button>
-    </div>
+    </>
   );
 }
